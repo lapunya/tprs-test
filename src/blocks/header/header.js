412ff7843de508f0d9c28b5d8d4d5b2f9478f947
Pavel <pavellapunov@inbox.ru>
2022-02-21 00:18:05 +0300

    add adaptive version without modal and pixel perfect

---
+++ src/blocks/header/header.js
@@ -0,0 +1,52 @@
+const header = document.querySelector('.header');
+const search = document.querySelector('.search');
+
+const menu = header.querySelector('.menu');
+const support = header.querySelector('.support')
+
+const menuBtn = header.querySelector('.header__menu-btn');
+const supportBtn = header.querySelector('.header__help-btn');
+
+const setHeaderStatic = () => {
+    if (window.scrollY > header.offsetTop) {
+        search.classList.add("search--scrolled");
+        header.classList.add("header--static");
+    } else {
+        search.classList.remove("search--scrolled");
+        header.classList.remove("header--static");
+    }
+};
+
+const toggleMenuBtn = () => {
+    if (menuBtn.classList.contains('active')) {
+        menuBtn.classList.remove('active');
+    } else {
+        menuBtn.classList.add('active');
+    }
+};
+
+const setMenuMode = () => {
+    if (menu.classList.contains('menu--active')) {
+        menu.classList.remove('menu--active');
+    } else {
+        menu.classList.add('menu--active');
+    }
+};
+
+const setSupportMode = () => {
+    if (support.classList.contains('support--active')) {
+        support.classList.remove('support--active');
+    } else {
+        support.classList.add('support--active');
+    }
+};
+
+const toggleMenu = () => {
+    toggleMenuBtn();
+    setMenuMode();
+}
+
+menuBtn.addEventListener('click', toggleMenu);
+supportBtn.addEventListener('click', setSupportMode);
+
+window.onscroll = setHeaderStatic;
---
+++ src/blocks/options/options.js
@@ -0,0 +1,12 @@
+const optionsBtns = document.querySelectorAll('.options__item-title');
+
+optionsBtns.forEach(function(el) {
+    let title = el;
+    title.addEventListener('click', function() {
+        if (title.classList.contains('active')) {
+            title.classList.remove('active');
+        } else {
+            title.classList.add('active');
+        }
+    })
+})
